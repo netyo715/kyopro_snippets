8987ddb9e889a19ed2a2147ec4dd80816ced3970
Sohei Nishimura <netyo715@gmail.com>
2021-04-11 20:24:04 +0900

    スニペット回り

--- snippets_builder.py
+++ snippets_builder.py
@@ -1,10 +1,24 @@
-l = []
+name = input("name: ")
+prefix = input("prefix: ")
+description = input("description: ")
+print("body: ")
+
+body = []
 while True:
     s = input()
     if s == "end":
         break
-    l.append(s)
+    body.append(s)
 
-for s in l:
-    o = s.replace("    ", "\\t")
-    print(f"\"{o}\",")
+with open(f"snippets_{name}", mode="w") as f:
+    f.write(f'"{name}":'+"{\n")
+    f.write(f'\t"prefix": "{prefix}",\n')
+    frow = body[0].replace("    ", "\\t")
+    f.write(f'\t"body":["{frow}",')
+    for row in body[1:]:
+        rrow = row.replace("    ", "\\t")
+        #print(f"\"{o}\",")
+        f.write(f'\n\t\t\t"{rrow}",')
+    f.write("],\n")
+    f.write(f'\t"description": "{description}"\n')
+    f.write("}\n")
